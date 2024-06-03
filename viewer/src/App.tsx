99ded0bda13c5465977df0e27467a4634ee821f0
Ralf Vogler <ralf.vogler@gmail.com>
2024-06-03 14:25:45 +0200

    viewer: load last 10 entries from data/db.json

--- viewer/src/App.tsx
+++ viewer/src/App.tsx
@@ -1,33 +1,41 @@
 import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = createSignal(0)
+// https://medium.com/@akshaykrdas001/how-to-fetch-data-from-local-json-file-and-render-it-to-html-document-with-using-vanilla-javascript-a0191a894f25
+// can load local images via <img> without webserver, but no local .js or .json files via <script>, import or fetch() -> bundler could inline db.json during build, but would still need to load */book.json dynamically or put everything in db.json
+// -> just use `npm run dev` for now
 
+// import json from './db.json' // bundler can import if file is copied to src
+// however, we need to serve */book.json anyway, so we also just fetch the overview json
+const db = await (await fetch('/db.json')).json()
+
+const [size, setSize] = createSignal(250) // book cover width
+
+const Book = (book: any) => {
+  return (
+    <div class="card">
+      <a href={book.id}>
+        <img src={book.img} alt={book.title} width={size()} /> <br />
+        {book.title}
+      </a> <br />
+      <a href={book.url} target="_blank" rel="noreferrer">
+        blinkist
+      </a>
+    </div>
+  )
+}
+
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <a href="https://github.com/vogler/blinkist-downloader"><h1>blinkist-downloader</h1></a>
       <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
+        Saved: {db.saved.length} |
+        Finished: {db.finished.length}
       </p>
+      <button onClick={() => setSize((size) => size - 20)}> - </button>
+      <button onClick={() => setSize((size) => size + 20)}> + </button>
+      { db.saved.slice(-10).map(Book) }
     </>
   )
 }
--- viewer/vite.config.ts
+++ viewer/vite.config.ts
@@ -3,4 +3,5 @@ import solid from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [solid()],
+  publicDir: '../data',
 })
